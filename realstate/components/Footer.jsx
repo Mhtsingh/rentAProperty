a23f926d8f762b8476dfeee60e0fd8a8413066da
Mohit Singh <mhtsingh952@gmail.com>
2022-02-09 18:34:34 +0100

    added serch pages and property pages

---
+++ realstate/components/Footer.jsx
@@ -0,0 +1,9 @@
+import {Box} from '@chakra-ui/react'
+
+const Footer =() => (
+    <Box textAlign="center" p="5" color="gray.500" borderTop="1px" borderColor="gray.100">
+        2021 Home
+    </Box>
+);
+
+export default Footer;
